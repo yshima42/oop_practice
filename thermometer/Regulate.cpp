5d2c462a7565fe675ade2de317541fcb1b88dc8c
yuki <yuki@yukis-MacBook-Air.local>
2022-09-07 17:37:31 +0900

    up

--- thermometer/Regulate.cpp
+++ thermometer/Regulate.cpp
@@ -4,12 +4,12 @@ void Regulate::run(Thermometer & t, Heater & h, int minTemp, int maxTemp) {
 	for (;;) {
 		while (t.Read() > minTemp) {
 			std::cout << "GOOOOD!" << std::endl;
-			sleep(1);
+			//sleep(1);
 		}
 		h.Engage();
 		while (t.Read() < maxTemp) {
 			std::cout << "GOOOOD!" << std::endl;
-			sleep(1);
+			//sleep(1);
 		}
 		h.Disengage();
 	};
